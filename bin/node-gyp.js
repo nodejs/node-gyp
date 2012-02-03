58c814d822041d3e8305fe793633d267b8f0ecda
Nathan Rajlich <nathan@tootallnate.net>
2012-02-02 23:04:28 -0800

    Progress...

--- bin/node-gyp.js
+++ bin/node-gyp.js
@@ -10,11 +10,29 @@ process.title = 'node-gyp'
  * Module dependencies.
  */
 
-var nopt = require('nopt')
-  , knownOpts = {
-        debug: Boolean
-      , verbose: Boolean
-    }
-  , opts = nopt(knownOpts)
+var fs = require('fs')
+  , path = require('path')
+  , usage = require('../lib/usage')
 
-console.log(opts)
+/**
+ * Ensure a command was given.
+ */
+
+if (process.argv.length < 3) {
+  usage.print().exit()
+}
+
+/**
+ * Process and execute the selected command.
+ */
+
+var command = process.argv[2]
+  , module = path.join(__dirname, '..', 'lib', command + '.js')
+
+fs.stat(module, function (err, stat) {
+  if (err || !stat || !stat.isFile()) {
+    console.error('Unknown command `%s`', command)
+    return usage.print().exit()
+  }
+  require(module)(process.argv)
+})
