434505ae095730a6c475b23f65cfad4ae96d7363
Nathan Rajlich <nathan@tootallnate.net>
2012-02-03 15:19:23 -0800

    Start on centralized logging.

--- bin/node-gyp.js
+++ bin/node-gyp.js
@@ -11,6 +11,9 @@ process.title = 'node-gyp'
  */
 
 var gyp = require('../lib/node-gyp')
+  , inspect = require('util').inspect
+  , ansi = require('ansi')
+  , cursor = ansi(process.stdout)
 
 /**
  * Process and execute the selected command.
@@ -18,6 +21,46 @@ var gyp = require('../lib/node-gyp')
 
 var prog = gyp()
 prog.parseArgv(process.argv)
+
+/**
+ * Set up logging handlers.
+ */
+
+prog.on('info', function () {
+  cursor.fg.green().write('info ')
+        .fg.grey().write(arguments[0] + ' ')
+        .reset()
+  for (var i=1, l=arguments.length; i<l; i++) {
+    cursor.write(arguments[i] + ' ')
+  }
+  cursor.write('\n')
+})
+
+if (prog.opts.verbose) {
+  prog.on('verbose', function () {
+    cursor.fg.blue().write('verbose ')
+          .fg.grey().write(arguments[0] + ' ')
+          .reset()
+    for (var i=1, l=arguments.length; i<l; i++) {
+      cursor.write(inspect(arguments[i]) + ' ')
+    }
+    cursor.write('\n')
+  })
+}
+
+prog.on('spawn', function (command, args, proc) {
+  cursor.fg.magenta().write('spawn ')
+        .fg.grey().write(command + ' ')
+        .reset().write(args.join(' ') + '\n')
+
+  proc.stdout.pipe(process.stdout, { end: false })
+  proc.stderr.pipe(process.stderr, { end: false })
+})
+
+/**
+ * Process and execute the selected command.
+ */
+
 if (!prog.command) {
   return prog.usageAndExit()
 }
@@ -26,8 +69,3 @@ prog.commands[prog.command](prog.argv, function (err) {
   if (err) throw err
 })
 
-prog.on('spawn', function (command, args, proc) {
-  //console.error('spawn', command, args)
-  proc.stdout.pipe(process.stdout, { end: false })
-  proc.stderr.pipe(process.stderr, { end: false })
-})
