2764a717f3e897882d0a71097e47b426a354145b
Nathan Rajlich <nathan@tootallnate.net>
2012-02-03 02:21:07 -0800

    Quick 'install' command implementation.

--- bin/node-gyp.js
+++ bin/node-gyp.js
@@ -10,29 +10,10 @@ process.title = 'node-gyp'
  * Module dependencies.
  */
 
-var fs = require('fs')
-  , path = require('path')
-  , usage = require('../lib/usage')
-
-/**
- * Ensure a command was given.
- */
-
-if (process.argv.length < 3) {
-  usage.print().exit()
-}
+var gyp = require('../lib/node-gyp')()
 
 /**
  * Process and execute the selected command.
  */
 
-var command = process.argv[2]
-  , module = path.join(__dirname, '..', 'lib', command + '.js')
-
-fs.stat(module, function (err, stat) {
-  if (err || !stat || !stat.isFile()) {
-    console.error('Unknown command `%s`', command)
-    return usage.print().exit()
-  }
-  require(module)(process.argv)
-})
+gyp.run(process.argv)
