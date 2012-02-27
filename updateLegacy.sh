4e9b06ba96b738a974e523a28e2148171eaadfc9
Nathan Rajlich <nathan@tootallnate.net>
2012-02-27 14:38:43 -0800

    Add stupid little helper script to update the `legacy` dir with the latest node dev files.

---
+++ updateLegacy.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env sh
+
+cp -fpv ~/node/common.gypi legacy/
+cp -fpv ~/node/tools/addon.gypi legacy/tools/
+cp -fpv ~/node/tools/gyp_addon legacy/tools/
+cp -rfpv ~/node/tools/gyp legacy/tools/
