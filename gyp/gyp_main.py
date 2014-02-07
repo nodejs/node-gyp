49650623c2f927dca2455a85dc31a72d00140a58
Mateus Caruccio <mateus@caruccio.com>
2014-02-06 23:44:51 -0200

    Import local gyp.py, then fallback to system
    Some hosts have an old gyp version, which causes an ImportError.
    It is better to import newer gyp.py first, then fallback to system-wide version.

--- gyp/gyp_main.py
+++ gyp/gyp_main.py
@@ -8,10 +8,10 @@ import sys
 
 # TODO(mark): sys.path manipulation is some temporary testing stuff.
 try:
+  import os.path
+  sys.path = [ os.path.join(os.path.dirname(sys.argv[0]), 'pylib') ] + sys.path
   import gyp
 except ImportError, e:
-  import os.path
-  sys.path.append(os.path.join(os.path.dirname(sys.argv[0]), 'pylib'))
   import gyp
 
 if __name__ == '__main__':
