fa5ce91dcf7b402a9f23dfa3705af0f9237fcb0b
Ivan Egorov <vany.egorov@gmail.com>
2017-01-09 20:02:01 +0000

    refs https://github.com/yarnpkg/yarn/issues/2286 refs https://github.com/yarnpkg/yarn/issues/2376
    allow long type as result of ExpandVariables call. along with str and int.
    skip long type(value) along with int.
    
    debug (buggy long type):
    [
      {"type(expanded)": "<type 'long'>", "expanded": "1483986851164", "value": "1483986851164", "phase": "0", "build_file": "binding.gyp"},
      {"type(expanded)": "<type 'long'>", "expanded": "1483986851164", "value": "1483986851164", "phase": "0", "build_file": "deps\\libexpat\\libexpat.gyp"}
    ]
    
    use isinstance(...) instead of type(...) and use basestring instead of string while typechecking;

--- gyp/pylib/gyp/input.py
+++ gyp/pylib/gyp/input.py
@@ -1220,11 +1220,11 @@ def ProcessVariablesAndConditionsInDict(the_dict, phase, variables_in,
 
   for key, value in the_dict.iteritems():
     # Skip "variables", which was already processed if present.
-    if key != 'variables' and type(value) is str:
+    if key != 'variables' and isinstance(value, basestring):
       expanded = ExpandVariables(value, phase, variables, build_file)
-      if type(expanded) not in (str, int):
+      if not isinstance(expanded, (basestring, int, long)):
         raise ValueError(
-              'Variable expansion in this context permits str and int ' + \
+              'Variable expansion in this context permits str and int/long ' + \
               'only, found ' + expanded.__class__.__name__ + ' for ' + key)
       the_dict[key] = expanded
 
@@ -1279,21 +1279,21 @@ def ProcessVariablesAndConditionsInDict(the_dict, phase, variables_in,
   for key, value in the_dict.iteritems():
     # Skip "variables" and string values, which were already processed if
     # present.
-    if key == 'variables' or type(value) is str:
+    if key == 'variables' or isinstance(value, basestring):
       continue
-    if type(value) is dict:
+    if isinstance(value, dict):
       # Pass a copy of the variables dict so that subdicts can't influence
       # parents.
       ProcessVariablesAndConditionsInDict(value, phase, variables,
                                           build_file, key)
-    elif type(value) is list:
+    elif isinstance(value, list):
       # The list itself can't influence the variables dict, and
       # ProcessVariablesAndConditionsInList will make copies of the variables
       # dict if it needs to pass it to something that can influence it.  No
       # copy is necessary here.
       ProcessVariablesAndConditionsInList(value, phase, variables,
                                           build_file)
-    elif type(value) is not int:
+    elif not isinstance(value, (int, long)):
       raise TypeError('Unknown type ' + value.__class__.__name__ + \
                       ' for ' + key)
 
