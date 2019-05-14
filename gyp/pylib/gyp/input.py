1597c84aadc619e828c30001e6e9e39906aace73
cclauss <cclauss@me.com>
2019-05-14 19:39:08 +0200

    test: use Travis CI to run tests on every pull request
    This is a second attempt at #1336 which got into a bad git-state...
    
    Use flake8 to find Python syntax errors and undefined names.  There are Python 3 syntax errors and many undefined names which may raise NameError at runtime. This PR runs flake8 runs in two passes: The first looks at critical issues in stop-the-build mode and the second looks at style violations in everything-is-a-warning mode.
    
    PR-URL: https://github.com/nodejs/node-gyp/pull/1752
    Reviewed-By: Rod Vagg <rod@vagg.org>

--- gyp/pylib/gyp/input.py
+++ gyp/pylib/gyp/input.py
@@ -1182,7 +1182,7 @@ def LoadVariablesFromVariablesDict(variables, the_dict, the_dict_key):
       if variable_name in variables:
         # If the variable is already set, don't set it.
         continue
-      if the_dict_key is 'variables' and variable_name in the_dict:
+      if the_dict_key == 'variables' and variable_name in the_dict:
         # If the variable is set without a % in the_dict, and the_dict is a
         # variables dict (making |variables| a varaibles sub-dict of a
         # variables dict), use the_dict's definition.
