fa0ed4aa420f1ae192ac2729347f0cc71256cb69
cclauss <cclauss@me.com>
2019-07-12 10:33:33 +0200

    build: more Python 3 compat, replace compile with ast
    Make Python 3 compatiblity changes so the code works in both Python 2
    and Python 3.  Especially, make changes required because the compiler
    module was removed in Python 3 in favor of the ast module that exists
    in both Python 2 and Python 3.
    
    PR-URL: https://github.com/nodejs/node-gyp/pull/1820
    Reviewed-By: Ben Noordhuis <info@bnoordhuis.nl>
    Reviewed-By: Rod Vagg <r@va.gg>
    Reviewed-By: Richard Lau <riclau@uk.ibm.com>

--- gyp/pylib/gyp/xcode_emulation.py
+++ gyp/pylib/gyp/xcode_emulation.py
@@ -1636,7 +1636,7 @@ def _HasIOSTarget(targets):
 def _AddIOSDeviceConfigurations(targets):
   """Clone all targets and append -iphoneos to the name. Configure these targets
   to build for iOS devices and use correct architectures for those builds."""
-  for target_dict in targets.itervalues():
+  for target_dict in targets.values():
     toolset = target_dict['toolset']
     configs = target_dict['configurations']
     for config_name, config_dict in dict(configs).items():
