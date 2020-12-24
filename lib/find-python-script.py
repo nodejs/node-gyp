f18491ea99287265c88d855da7b5de6173b45281
owl-from-hogvarts <transftank@mail.ru>
2020-12-24 12:37:01 +0300

    lib: reinvent find-python.js script
    now this script using promises and es6 classes
    !!!API HASN'T CHANGE
    add documentation
    make better logging (colorized output)
    make more convenient error handling (one function that catch all errors)

--- lib/find-python-script.py
+++ lib/find-python-script.py
@@ -1,11 +1,7 @@
-import sys, codecs;
+import sys;
 
 if (sys.stdout.encoding != "utf-8" and sys.platform == "win32"):
-    if sys.version_info > (3, 7):
-        sys.stdout.reconfigure(encoding='utf-8')
-        print(sys.executable)
-    else:
-        sys.stdout = codecs.getwriter("utf8")(sys.stdout)
-        print(sys.executable.decode("cp1251"))
+    sys.stdout.reconfigure(encoding='utf-8')
+    print(sys.executable)
 else:
     print(sys.executable)
