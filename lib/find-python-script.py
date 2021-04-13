f26edde6bf5b2ecef19ce814e951858170aa3243
Christian Clauss <cclauss@me.com>
2021-04-13 10:38:08 +0300

    lib: remove unnecessary "else"

--- lib/find-python-script.py
+++ lib/find-python-script.py
@@ -1,7 +1,4 @@
 import sys
-
-if (sys.stdout.encoding != "utf-8" and sys.platform == "win32"):
+if sys.stdout.encoding != "utf-8" and sys.platform == "win32":
     sys.stdout.reconfigure(encoding='utf-8')
-    print(sys.executable)
-else:
-    print(sys.executable)
+print(sys.executable)
