37814eca6d1d96d38d898a3954c918db1b6d5ca9
owl-from-hogvarts <transftank@mail.ru>
2021-06-03 20:44:03 +0300

    fix(lib): use new `find-python-script.py`

--- lib/find-python-script.py
+++ lib/find-python-script.py
@@ -1,11 +1,4 @@
-import sys, codecs;
-
-if (sys.stdout.encoding != "utf-8" and sys.platform == "win32"):
-    if sys.version_info > (3, 7):
-        sys.stdout.reconfigure(encoding='utf-8')
-        print(sys.executable)
-    else:
-        sys.stdout = codecs.getwriter("utf8")(sys.stdout)
-        print(sys.executable.decode("cp1251"))
-else:
-    print(sys.executable)
+import sys
+if sys.stdout.encoding != "utf-8" and sys.platform == "win32":
+    sys.stdout.reconfigure(encoding='utf-8')
+print(sys.executable)
