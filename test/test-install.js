5a659af87552386be3473c6eaaf09ddef4af058a
David Sanders <dsanders11@ucsbalum.com>
2023-05-07 08:12:29 -0700

    test: parallel installs
    Signed-off-by: David Sanders <dsanders11@ucsbalum.com>

--- test/test-install.js
+++ test/test-install.js
@@ -1,8 +1,16 @@
 'use strict'
 
 const { test } = require('tap')
-const { test: { install } } = require('../lib/install')
+const path = require('path')
+const os = require('os')
+const util = require('util')
+const { test: { download, install } } = require('../lib/install')
+const rimraf = require('rimraf')
+const gyp = require('../lib/node-gyp')
 const log = require('npmlog')
+const semver = require('semver')
+const stream = require('stream')
+const streamPipeline = util.promisify(stream.pipeline)
 
 log.level = 'error' // we expect a warning
 
@@ -43,4 +51,80 @@ test('EACCES retry once', async (t) => {
       t.ok(true)
     }
   }
+})
+
+// only run these tests if we are running a version of Node with predictable version path behavior
+const skipParallelInstallTests = process.env.FAST_TEST ||
+  process.release.name !== 'node' ||
+  semver.prerelease(process.version) !== null ||
+  semver.satisfies(process.version, '<10')
+
+async function parallelInstallsTest (t, fs, devDir, prog) {
+  if (skipParallelInstallTests) {
+    return t.skip('Skipping parallel installs test due to test environment configuration')
+  }
+
+  t.tearDown(async () => {
+    await util.promisify(rimraf)(devDir)
+  })
+
+  const expectedDir = path.join(devDir, process.version.replace(/^v/, ''))
+  await util.promisify(rimraf)(expectedDir)
+
+  await Promise.all([
+    install(fs, prog, []),
+    install(fs, prog, []),
+    install(fs, prog, []),
+    install(fs, prog, []),
+    install(fs, prog, []),
+    install(fs, prog, []),
+    install(fs, prog, []),
+    install(fs, prog, []),
+    install(fs, prog, []),
+    install(fs, prog, [])
+  ])
+}
+
+test('parallel installs (ensure=true)', async (t) => {
+  const fs = require('graceful-fs')
+  const devDir = await util.promisify(fs.mkdtemp)(path.join(os.tmpdir(), 'node-gyp-test-'))
+
+  const prog = gyp()
+  prog.parseArgv([])
+  prog.devDir = devDir
+  prog.opts.ensure = true
+  log.level = 'warn'
+
+  await parallelInstallsTest(t, fs, devDir, prog)
+})
+
+test('parallel installs (ensure=false)', async (t) => {
+  const fs = require('graceful-fs')
+  const devDir = await util.promisify(fs.mkdtemp)(path.join(os.tmpdir(), 'node-gyp-test-'))
+
+  const prog = gyp()
+  prog.parseArgv([])
+  prog.devDir = devDir
+  prog.opts.ensure = false
+  log.level = 'warn'
+
+  await parallelInstallsTest(t, fs, devDir, prog)
+})
+
+test('parallel installs (tarball)', async (t) => {
+  const fs = require('graceful-fs')
+  const devDir = await util.promisify(fs.mkdtemp)(path.join(os.tmpdir(), 'node-gyp-test-'))
+
+  const prog = gyp()
+  prog.parseArgv([])
+  prog.devDir = devDir
+  prog.opts.tarball = path.join(devDir, 'node-headers.tar.gz')
+  log.level = 'warn'
+
+  await streamPipeline(
+    (await download(prog, 'https://nodejs.org/dist/v16.0.0/node-v16.0.0.tar.gz')).body,
+    fs.createWriteStream(prog.opts.tarball)
+  )
+
+  await parallelInstallsTest(t, fs, devDir, prog)
 })
