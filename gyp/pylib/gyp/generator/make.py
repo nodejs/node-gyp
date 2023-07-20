4285735f856ae174770edcbfed0ac8af6e7c7b8a
Christian Clauss <cclauss@me.com>
2023-07-20 15:04:39 +0200

    Fix Python lint error by using an f-string (#2886)

--- gyp/pylib/gyp/generator/make.py
+++ gyp/pylib/gyp/generator/make.py
@@ -1991,11 +1991,8 @@ $(obj).$(TOOLSET)/$(TARGET)/%%.o: $(obj)/%%%s FORCE_DO_CMD
                 and "product_dir" not in spec
                 and self.toolset == "target"
             ):
-                # On mac, products are created in install_path immediately.
-                assert install_path == self.output, "{} != {}".format(
-                    install_path,
-                    self.output,
-                )
+                # On macOS, products are created in install_path immediately.
+                assert install_path == self.output, f"{install_path} != {self.output}"
 
             # Point the target alias to the final binary output.
             self.WriteMakeRule(
