2407f6892b5008bebf2100d5ac254daa50e18ce6
Luke Karrys <luke@lukekarrys.com>
2025-08-15 10:20:52 -0700

    add tests

--- test/test-options.js
+++ test/test-options.js
@@ -8,19 +8,28 @@ describe('options', function () {
   it('options in environment', () => {
     // `npm test` dumps a ton of npm_config_* variables in the environment.
     Object.keys(process.env)
-      .filter((key) => /^npm_config_/.test(key))
+      .filter((key) => /^npm_config_/.test(key) || /^npm_package_config_node_gyp_/.test(key))
       .forEach((key) => { delete process.env[key] })
 
     // in some platforms, certain keys are stubborn and cannot be removed
     const keys = Object.keys(process.env)
-      .filter((key) => /^npm_config_/.test(key))
+      .filter((key) => /^npm_config_/.test(key) || /^npm_package_config_node_gyp_/.test(key))
       .map((key) => key.substring('npm_config_'.length))
-      .concat('argv', 'x')
+      .concat('argv', 'x', 'y', 'foo')
+
+    // Environment variables with the following prefixes should be added to opts.
+    // - `npm_config_` for npm versions before v11.
+    // - `npm_package_config_node_gyp_` for npm versions 11 and later.
 
     // Zero-length keys should get filtered out.
     process.env.npm_config_ = '42'
+    process.env.npm_package_config_node_gyp_ = '42'
     // Other keys should get added.
     process.env.npm_config_x = '42'
+    process.env.npm_config_y = '41'
+    process.env.npm_package_config_node_gyp_foo = '42'
+    // Package config should take precedence over npm_config_ keys.
+    process.env.npm_package_config_node_gyp_y = '42'
     // Except loglevel.
     process.env.npm_config_loglevel = 'debug'
 
@@ -28,6 +37,9 @@ describe('options', function () {
     g.parseArgv(['rebuild']) // Also sets opts.argv.
 
     assert.deepStrictEqual(Object.keys(g.opts).sort(), keys.sort())
+    assert.strictEqual(g.opts['x'], '42')
+    assert.strictEqual(g.opts['y'], '42')
+    assert.strictEqual(g.opts['foo'], '42')
   })
 
   it('options with spaces in environment', () => {
